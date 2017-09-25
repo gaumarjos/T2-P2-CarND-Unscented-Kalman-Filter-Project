30e361b036443a93848c251ea9687f9e08e54c31
Stefano Salati <stef.salati@gmail.com>
2017-09-25 11:43:53 +0200

    Initialization and basic function calls

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -66,6 +66,108 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
   Complete this function! Make sure you switch between lidar and radar
   measurements.
   */
+  
+  /*****************************************************************************
+   *  Initialization
+   ****************************************************************************/
+  if (!is_initialized_) {
+  
+    x_ = VectorXd(5);
+    
+    if (meas_package.sensor_type_ == MeasurementPackage::RADAR)
+    {
+      float rho = meas_package.raw_measurements_[0];
+      float theta = meas_package.raw_measurements_[1];
+      float rho_dot = meas_package.raw_measurements_[2];
+      x_(0) = rho * cos(theta);
+      x_(1) = rho * sin(theta);
+      x_(2) = 0;
+      x_(3) = 0;
+      x_(4) = 0;
+    }
+    else if (meas_package.sensor_type_ == MeasurementPackage::LASER)
+    {
+      x_ << meas_package.raw_measurements_[0], meas_package.raw_measurements_[1], 0, 0, 0;
+    }
+    
+    //initial state covariance matrix P
+	  P_ = MatrixXd(5, 5);
+	  P_ << 1, 0, 0, 0, 0,
+			    0, 1, 0, 0, 0,
+			    0, 0, 1, 0, 0,
+			    0, 0, 0, 1, 0,
+			    0, 0, 0, 0, 1;
+		
+    time_us_ = meas_package.timestamp_;
+    
+    // done initializing, no need to predict or update
+    cout << "Initialized, x: " << x_ << endl;
+    is_initialized_ = true;
+    return;
+  }
+
+  /*****************************************************************************
+   *  Prediction
+   ****************************************************************************/
+
+  /**
+   TODO:
+     * Update the state transition matrix F according to the new elapsed time.
+      - Time is measured in seconds.
+     * Update the process noise covariance matrix.
+     * Use noise_ax = 9 and noise_ay = 9 for your Q matrix.
+   */
+   
+   //compute the time elapsed between the current and previous measurements
+	float dt = (meas_package.timestamp_ - time_us_) / 1000000.0;
+	time_us_ = meas_package.timestamp_;
+	
+	/**
+	ekf_.F_ = MatrixXd(4,4);
+	ekf_.F_ << 1, 0, dt, 0,
+			       0, 1, 0,  dt,
+			       0, 0, 1,  0,
+			       0, 0, 0,  1;
+	
+	float dt2 = dt * dt;
+	float dt3 = dt2 * dt;
+	float dt4 = dt3 * dt;
+	float dt4_4 = dt4 / 4;
+	float dt3_2 = dt3 / 2;
+  float noise_ax = 9;
+	float noise_ay = 9;
+  ekf_.Q_ = MatrixXd(4,4);
+  ekf_.Q_ << dt4_4*noise_ax, 0,              dt3_2*noise_ax, 0,
+			       0,              dt4_4*noise_ay, 0,              dt3_2*noise_ay,
+			       dt3_2*noise_ax, 0,              dt2*noise_ax,   0,
+			       0,              dt3_2*noise_ay, 0,              dt2*noise_ay;
+  */
+  Prediction(dt);
+
+  /*****************************************************************************
+   *  Update
+   ****************************************************************************/
+
+  /**
+   TODO:
+     * Use the sensor type to perform the update step.
+     * Update the state and covariance matrices.
+   */
+   
+  if (meas_package.sensor_type_ == MeasurementPackage::RADAR) {
+    // Radar updates
+    UpdateRadar(meas_package);
+  }
+  else {
+    // Laser updates
+    UpdateLidar(meas_package);
+  }
+
+  // print the output
+  // cout << "x_ = " << x_ << endl;
+  // cout << "P_ = " << P_ << endl;
+  cout << meas_package.raw_measurements_ << endl;
+    
 }
 
 /**
